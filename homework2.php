b909840e7074fe722e4a523f375203e86d2a6dab
Elliott <emilybeeley2020@gmail.com>
2025-02-08 21:17:42 -0500

    fix. html to php

--- homework2.php
+++ homework2.php
@@ -1,11 +1,5 @@
-<!doctype html>
-<html lang="en-US">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width" />
-    <title>Homework 2</title>
-  </head>
-  <body>
-    <p>Fun fact: Jupiter has 95 moons!</p>
-  </body>
-</html>
+<?php
+
+
+
+?>
