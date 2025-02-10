5394775f03ab6a3565d793bef3b46edb681a07b9
Elliott <emilybeeley2020@gmail.com>
2025-02-10 17:50:51 -0500

    feat.html boilerplate

--- homework2.php
+++ homework2.php
@@ -1,5 +1,22 @@
-<?php
+<!doctype html>
 
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Homework 2</title>
+  </head>
 
+  <body>
 
-?>
+    <h1>Welcome!<h1>
+
+        <?php
+
+            echo "hello";
+
+        ?>
+
+  </body>
+
+</html>
